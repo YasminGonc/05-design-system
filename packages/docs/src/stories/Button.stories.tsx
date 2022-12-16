d35593f63a2289aef638d0de8c9cc45edd03b453
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-12-16 09:38:36 -0300

    Storybook dark theme

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,18 +1,19 @@
 import { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
-export default { //configuração global do botão
-    title: 'Button',
-    component: Button,
-    args: {
-        children: 'Enviar'
-    },
+export default {
+  // configuração global do botão
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
 
 export const Big: StoryObj<ButtonProps> = {
-    args: {
-        size: 'big',
-    },
+  args: {
+    size: 'big',
+  },
 }
