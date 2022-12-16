3a055a718850ad285a6d64888cd0fdc9b69456cd
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-12-15 21:52:52 -0300

    First story

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -120,15 +120,29 @@ var {
 });
 
 // src/index.tsx
-import { jsx } from "react/jsx-runtime";
 var Button = styled("button", {
   fontFamily: "$default",
-  backgroundColor: "$ignite500",
-  borderRadius: "$md"
+  backgroundColor: "$ignite300",
+  border: 0,
+  borderRadius: "$sm",
+  fontWeight: "$bold",
+  color: "$white",
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4"
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6"
+      }
+    }
+  },
+  defaultVariants: {
+    size: "small"
+  }
 });
-function App() {
-  return /* @__PURE__ */ jsx(Button, { children: "Hello World" });
-}
 export {
-  App
+  Button
 };
