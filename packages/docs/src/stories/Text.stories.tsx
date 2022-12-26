3a415e6ad18ca66149f48e9d2ef802a4ae7970a6
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-12-26 10:24:43 -0300

    storybook improvements

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum molestias dolor reiciendis delectus, natus nihil, assumenda ea provident aliquam debitis expedita, fugit eius suscipit eos. Reiciendis velit autem ab nostrum?',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
